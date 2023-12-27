36980fb6019354d83707f20a99d2b11af042d821
Gabriel Echeverria <gabrielecheverria@ravn.co>
2023-12-27 11:17:42 -0600

    Initial Commit

---
+++ BlendMode/BlendModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlendModeApp.swift
+//  BlendMode
+//
+//  Created by RAVN on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct BlendModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
